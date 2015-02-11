6a29affca3a40c67aad6cc5db00dc8afa571b46c
dialys <snzf2326>
2015-02-11 15:08:08 +0100

    Average

--- trackGrades/trackGrades.cs
+++ trackGrades/trackGrades.cs
@@ -33,6 +33,26 @@ namespace trackGrades
             }
             return average;
         }
-    }
 
+        public static double getAverage2(int[,]array)
+        {
+            //int[,] grades = new int[,] {{1, 82, 74, 89, 100},
+            //{2, 93, 96, 85, 86},
+            //{3, 83, 72, 95, 89},
+            //{4, 91, 98, 79, 88}};
+            int lastStudent = array.GetUpperBound(0);
+            int lastGrade = array.GetUpperBound(1);
+            double average = 0.0;
+            int total;
+            for (int row = 0; row <= lastStudent; row++)
+            {
+                total = 0;
+                for (int col = 0; col <= lastGrade; col++)
+                    total += array[row, col];
+                average = total / lastGrade;
+                //Console.WriteLine("Average grade: " + average);
+            }
+            return average;
+        }
+    }
 }

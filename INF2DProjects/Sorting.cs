9f6648a75625c1eab281b6e123dd586ba65edfff
dialys <snzf2326>
2015-02-11 10:26:13 +0100

    Sorting done

--- INF2DProjects/Sorting.cs
+++ INF2DProjects/Sorting.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace INF2DProjects
 {
-    class Sorting
+    public static class Sorting
     {
+       
+        //Bubblesort function
+        public static void BubbleSort<A>(this A[] array) where A : IComparable<A>
+        {
+            bool isDone = false;
+            for (int outer = array.Length - 1; outer >= 1; outer--)
+            {
+                for (int inner = 0; inner <= outer - 1; inner++)
+                {
+                    if (array[inner].CompareTo(array[outer]) > 0)
+                    {
+                        var temp = array[inner];
+                        array[inner] = array[inner + 1];
+                        array[inner + 1] = temp;
+                        isDone = true;
+                    }
+                }
+                if (!isDone)
+                {
+                    break;
+                }
+            }
+        }
     }
 }

cdbbd6b31777464a79a2062a211dea1b4ddb73a5
dialys <snzf2326>
2015-02-12 10:06:22 +0100

    timer

---
+++ INF2DProjects/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2DProjects
+{
+    class Timer
+    {
+        //Create the stopwatch
+        Stopwatch sw = new Stopwatch();
+
+        //Method to start the stopwatch
+        public void start()
+        {
+            sw.Reset();
+            sw.Start();
+        }
+        
+        //Method to stop the stopwatch
+        public void stop()
+        {
+            sw.Stop();
+        }
+
+        //Method to return the elapsed time
+        public string getTime()
+        {
+            TimeSpan ts = sw.Elapsed;
+            return ts.ToString();
+        }
+
+    }
+}

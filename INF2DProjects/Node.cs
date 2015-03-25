1e5351927cc5003f02831ab0caa9357f4275e451
eboer <Thunder377@hotmail.com>
2015-03-25 13:59:29 +0100

    commentednode

--- INF2DProjects/Node.cs
+++ INF2DProjects/Node.cs
@@ -8,15 +8,17 @@ namespace INF2DProjectsDLL
 {
     public class Node
     {
-        public Object Element;
-        public Node Link;
+        public Object Element; // Node data
+        public Node Link; // Next node
 
+        // Default Constructor
         public Node()
         {
             Element = null;
             Link = null;
         }
 
+        // Parameterized constructor
         public Node(Object theElement)
         {
             Element = theElement;

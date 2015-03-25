a82c15a4cd34b32c84b55558e41a876daded6fab
eboer <Thunder377@hotmail.com>
2015-03-25 13:01:18 +0100

    Nodeklaar

--- INF2DProjects/Node.cs
+++ INF2DProjects/Node.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace INF2DProjectsDLL
 {
-    class Node
+    public class Node
     {
+        public Object Element;
+        public Node Link;
+
+        public Node()
+        {
+            Element = null;
+            Link = null;
+        }
+
+        public Node(Object theElement)
+        {
+            Element = theElement;
+            Link = null;
+        }
     }
 }

5ca5b4108c578452999414e22b4aa4af640054fe
eboer <Thunder377@hotmail.com>
2015-03-25 14:29:57 +0100

    commentedLinkedList

--- INF2DProjects/LinkedList.cs
+++ INF2DProjects/LinkedList.cs
@@ -9,53 +9,59 @@ namespace INF2DProjectsDLL
     public class LinkedList
     {
         protected Node header;
+
+        // Constructor
         public LinkedList()
         {
-            header = new Node("header");
+            header = new Node("header"); // Header node
         }
 
+        // Find node method
         private Node Find(Object item)
         {
-            Node current = new Node();
-            current = header;
-            while (current.Element != item)
-                current = current.Link;
-            return current;
+            Node current = new Node(); // Create node object
+            current = header; // Assign current as header node
+            while (current.Element != item) // Search for "after" node
+                current = current.Link; // Set new node's link field to found node
+            return current; // Return found node
         }
 
+        // Insert node method
         public void Insert(Object newItem, Object after)
         {
-            Node current = new Node();
-            Node newNode = new Node(newItem);
-            current = Find(after);
-            newNode.Link = current.Link;
-            current.Link = newNode;
+            Node current = new Node(); // Store current node
+            Node newNode = new Node(newItem); // Store new node
+            current = Find(after); // after link field
+            newNode.Link = current.Link; // Assign new node link field with after node link field
+            current.Link = newNode; // Make reference to new node
         }
 
+        // Find node before target method
         private Node FindPrevious(Object n)
         {
-            Node current = header;
-            while (!(current.Link == null) && (current.Link.
-            Element != n))
-                current = current.Link;
-            return current;
+            Node current = header; // Start at header
+            while (!(current.Link == null) && (current.Link.Element != n)) // Check each node element field until node before target is found
+                current = current.Link; // Next node
+            return current; // Return target
         }
 
+        // Remove node method
         public void Remove(Object n)
         {
-            Node p = FindPrevious(n);
-            if (!(p.Link == null))
-                p.Link = p.Link.Link;
+            Node p = FindPrevious(n); // Store node before target
+            if (!(p.Link == null)) // If there is a next node
+                p.Link = p.Link.Link; // Set link to 2 nodes ahead
         }
 
+        // Print data of each node method
         public void PrintList()
         {
             Node current = new Node();
-            current = header;
-            while (!(current.Link == null))
+            current = header; // Start at header
+            while (!(current.Link == null)) // As long as there is a next node
             {
-                Console.WriteLine(current.Link.Element);
-                current = current.Link;
+                Console.WriteLine(current.Link.Element); // Print current node's data
+                current = current.Link; // Next node
             }
         }
     }

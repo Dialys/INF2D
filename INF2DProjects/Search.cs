fac76e93ef73b416d2a11ce8a6ec2b52080f1aa8
dialys <snzf2326>
2015-03-30 10:06:12 +0200

    Search

---
+++ INF2DProjects/Search.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2DProjectsDLL
+{
+    class Search
+    {
+
+        /// Uses a sequentialsearch on the specified array
+        public Boolean SequentialSearch<T>(T[] objs, T obj) where T : IComparable<T>
+        {
+            for (int i = 0; i < objs.Length - 1; i++)
+            {
+                T temp = (T) objs.GetValue(i);
+                if (temp.CompareTo(obj) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// Uses a binarysearch on the specified array
+        public Boolean BinarySearch<T>(T[] objs, T obj) where T : IComparable<T>
+        {
+            int upperBound, lowerBound, mid;
+            upperBound = objs.Length - 1;
+            lowerBound = 0;
+
+            while (lowerBound <= upperBound)
+            {
+                mid = (upperBound + lowerBound) / 2;
+                if (objs[mid].CompareTo(obj) == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (obj.CompareTo(objs[mid]) == 1)
+                    {
+                        upperBound = mid - 1;
+                    }
+                    else
+                    {
+                        lowerBound = mid + 1;
+                    }
+                }
+            }
+            return false;
+        }
+    
+    }
+}

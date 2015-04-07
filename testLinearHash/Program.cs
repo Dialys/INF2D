d2dee618c6bfa534f3d6a4809dad1105f0e16082
dialys <snzf2326>
2015-04-07 14:37:59 +0200

    LinearHash

--- testLinearHash/Program.cs
+++ testLinearHash/Program.cs
@@ -11,6 +11,24 @@ namespace testLinearHash
     {
         static void Main(string[] args)
         {
+            //Make a linearhash
+            LinearHash linearHash = new LinearHash();
+
+            //Add data to the linearHash
+            linearHash.add("test0", "test12");
+            linearHash.add("test1", "test13");
+            linearHash.add("test2", "test14");
+            linearHash.add("test3", "test15");
+
+            //Get the posistions array of the linearHash
+            linearHash.getPositions();
+
+            //Check if the getValue method works
+            Console.WriteLine(linearHash.getValue("test1"));
+
+            //Check if the getHashTableSize method works
+            Console.WriteLine(linearHash.getHashTableSize());
+            Console.ReadLine();
 
 
         }

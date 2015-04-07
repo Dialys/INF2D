c23b043d2b37fc1d318e4046f056cbac5c916d93
Harrie Kiezebrink <harrie.kiezebrink@hotmail.com>
2015-04-07 11:14:29 +0200

    Add testHarrie project

---
+++ testHarrie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testHarrie
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}

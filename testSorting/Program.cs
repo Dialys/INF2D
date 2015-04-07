cfa55d023432771588f9e7abacc37c54961e6627
dialys <snzf2326>
2015-04-07 14:55:18 +0200

    sort comments

--- testSorting/Program.cs
+++ testSorting/Program.cs
@@ -17,16 +17,19 @@ namespace testSorting{
             //Reads the input of the user to see which algorithm to use
             string line = Console.ReadLine();
 
+            //if you want the bubble algorithm
             if (line == "bubble")
             {
                 bubbleFunction();
                 Console.ReadLine();
             }
+            //if you wnat the select algorithm
             else if (line == "select")
             {
                 selectFunction();
                 Console.ReadLine();
             }
+            //if you want the insert algorithm
             else if (line == "insert")
             {
                 insertFunction();
@@ -50,11 +53,9 @@ namespace testSorting{
                 Console.Write(str + " ");
             }
             Console.WriteLine();
-            timer.Start();
+         
+           //Sort the string array with the bubblesort
             Sorting.BubbleSort<string>(bubbleArray);
-            timer.Stop();
-
-            Console.WriteLine(timer.getTime());
             Console.Write("Sorted Array: ");
             foreach (string str in bubbleArray)
             {
@@ -68,11 +69,9 @@ namespace testSorting{
             }
 
             Console.WriteLine();
-            timer.Start();
-            Sorting.BubbleSort<int>(bubbleArray2);
-            timer.Stop();
-            Console.WriteLine(timer.getTime());
-
+           //Sort the integer array with the bubblesort 
+           Sorting.BubbleSort<int>(bubbleArray2);
+           
             Console.Write("Sorted Array: ");
             foreach (int i in bubbleArray2)
             {
@@ -84,6 +83,7 @@ namespace testSorting{
 
        static void selectFunction()
        {
+           //Make an integer and a string array
            string[] selectArray = { "asd", "xvf", "sdgaah", "wqrq", "xcv", "grs" };
            int[] selectArray2 = { 8, 5, 2, 6, 7, 2, 1 };
            Timer timer = new Timer();
@@ -96,11 +96,9 @@ namespace testSorting{
            }
 
            Console.WriteLine();
-           timer.Start();
+           //Sort the string array with the selectionsort
            Sorting.SelectionSort<string>(selectArray);
-           timer.Stop();
-           Console.WriteLine("Time to execute: " + timer.getTime());
-          
+                     
            Console.Write("Sorted array: ");
            foreach (string str in selectArray)
            {
@@ -117,11 +115,9 @@ namespace testSorting{
            }
 
            Console.WriteLine();
-
-           timer.Start();
+           //Sort the integer array with the selectionsort
            Sorting.SelectionSort<int>(selectArray2);
-           timer.Stop();
-           Console.WriteLine("Time to execute: " + timer.getTime());
+           
            Console.Write("Sorted Array: ");
            foreach (int i in selectArray2)
            {
@@ -133,6 +129,7 @@ namespace testSorting{
 
        static void insertFunction()
        {
+           //Make a integer array and an string array
            string[] insertArray = { "asd", "xvf", "sdgaah", "wqrq", "xcv", "grs" };
            int[] insertArray2 = { 8, 5, 2, 6, 7, 2, 1 };
            Timer timer = new Timer();
@@ -144,11 +141,10 @@ namespace testSorting{
            }
 
            Console.WriteLine();
-           timer.Start();
+           
+           //Sort the string array with the insertionsort
            Sorting.InsertionSort<string>(insertArray);
-           timer.Stop();
-           Console.WriteLine("Time to execute: " + timer.getTime());
-
+         
            Console.Write("Sorted array: ");
            foreach (string str in insertArray)
            {
@@ -165,10 +161,9 @@ namespace testSorting{
            }
 
            Console.WriteLine();
-           timer.Start();
+
+           //Sort the integer array with the insertionsort
            Sorting.InsertionSort<int>(insertArray2);
-           timer.Stop();
-           Console.WriteLine("Time to execute: " + timer.getTime()); 
            Console.Write("Sorted array: ");
            foreach (int i in insertArray2)
            {

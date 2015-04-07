385e8eb9d403636c6f392b61c8fdd97aaa06b4eb
dialys <snzf2326>
2015-04-07 12:47:53 +0200

    testLinkedList

--- testLinkedList/Program.cs
+++ testLinkedList/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using INF2DProjectsDLL;
 
 namespace testLinkedList
 {
@@ -10,6 +11,25 @@ namespace testLinkedList
     {
         static void Main(string[] args)
         {
+            LinkedList LinkedList = new LinkedList();
+            ListIter iter = new ListIter(LinkedList);
+
+            iter.InsertAfter("Test1");
+            iter.InsertAfter("Test2");
+            iter.InsertAfter("Tets3");
+            iter.InsertAfter("Test4");
+            Console.WriteLine(iter.GetCurrent());
+            iter.InsertAfter("Test5");
+            iter.InsertBefore("Test6");
+            iter.InsertAfter("Test7");
+            iter.Remove();
+            LinkedList.PrintList();
+            Console.WriteLine(LinkedList.getFirst());
+
+           
+            Console.ReadLine();
+
+
         }
     }
 }

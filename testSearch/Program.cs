9361e52f9a5e43e0253497982bc1985a5aa61993
dialys <snzf2326>
2015-04-07 13:47:38 +0200

    f

--- testSearch/Program.cs
+++ testSearch/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using INF2DProjectsDLL;
 
 namespace testSearch
 {
@@ -10,6 +11,13 @@ namespace testSearch
     {
         static void Main(string[] args)
         {
+            int[] num = new int[8] { 4,8,12,81,38,65,48, 72};
+
+            Search src = new Search();
+
+            Console.WriteLine(src.BinarySearch<int>(num, 5));
+            Console.WriteLine(src.SequentialSearch<int>(num, 39));
+            Console.ReadLine();
         }
     }
 }

734bd2c2fcc20b266b0f7211b125ab5bb967bb6f
dialys <snzf2326>
2015-04-07 14:49:18 +0200

    Search Comments

--- testSearch/Program.cs
+++ testSearch/Program.cs
@@ -11,10 +11,13 @@ namespace testSearch
     {
         static void Main(string[] args)
         {
+           //Creat an array of integers to search in
             int[] num = new int[8] { 4,8,12,81,38,65,48, 72};
 
+            //Make a search object
             Search src = new Search();
 
+            //Test the search methods
             Console.WriteLine(src.BinarySearch<int>(num, 5));
             Console.WriteLine(src.SequentialSearch<int>(num, 39));
             Console.ReadLine();

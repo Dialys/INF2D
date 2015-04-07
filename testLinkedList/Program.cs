fdc231d48f6a9f54ab0f3b67085fa25977a6c567
dialys <snzf2326>
2015-04-07 14:48:23 +0200

    LinkedList

--- testLinkedList/Program.cs
+++ testLinkedList/Program.cs
@@ -11,22 +11,29 @@ namespace testLinkedList
     {
         static void Main(string[] args)
         {
+            //Make a LinkedList and an iterator for the linkedlist
             LinkedList LinkedList = new LinkedList();
             ListIter iter = new ListIter(LinkedList);
 
+            //Add data with the iterator in the linkedlist
             iter.InsertAfter("Test1");
             iter.InsertAfter("Test2");
             iter.InsertAfter("Tets3");
             iter.InsertAfter("Test4");
+            
+            //Test if the getCurrent method works
             Console.WriteLine(iter.GetCurrent());
             iter.InsertAfter("Test5");
             iter.InsertBefore("Test6");
             iter.InsertAfter("Test7");
+           
+            //Test if the remove method works
             iter.Remove();
+
+            //Print the list of the linkedlist
             LinkedList.PrintList();
             Console.WriteLine(LinkedList.getFirst());
-
-           
+            
             Console.ReadLine();
 
 
